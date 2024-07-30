bbd21cf1c525617f17be7ecdd066397cd17e7c51
KsHerklo <klaus.herklotz@sintec.de>
2024-07-30 14:41:12 +0200

    Move added

--- src/main/java/de/sintec/rot_gegen_blau/MoveController.java
+++ src/main/java/de/sintec/rot_gegen_blau/MoveController.java
@@ -3,7 +3,9 @@ package de.sintec.rot_gegen_blau;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MoveController {
     @Autowired
     MoveService moveService;
